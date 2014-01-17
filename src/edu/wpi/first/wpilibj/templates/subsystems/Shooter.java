3acf019df5fef0c52cd4e891dc502d33daa9aae0
grr340 <grr340@grr340>
2014-01-16 20:51:49 -0500

    Added Shooter subsystem and started on ShootHuman command (no auto support) TODO: Make it so user can't move arm too far as to break it finish command

--- src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
@@ -5,6 +5,8 @@
  */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,9 +16,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Shooter extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+        private Solenoid trigger = new Solenoid(1);
+        private Victor puller = new Victor(6);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+
+    }
+    public void pullBack(double Speed) {
+        puller.set(Speed);
+    }
+    public void shoot() {
+        trigger.set(false);
     }
 }
