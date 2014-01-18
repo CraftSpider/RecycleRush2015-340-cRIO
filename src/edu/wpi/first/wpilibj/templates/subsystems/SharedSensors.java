02edd5d308ff087663cb417afeb21321cd387875
Tyler Pawlaczyk <armgeek2357@gmail.com>
2014-01-18 17:36:19 -0500

    Added the preliminary version of the Shared Sensors subsystem.

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/SharedSensors.java
@@ -0,0 +1,62 @@
+/**
+ * @author Tyler Pawlaczyk
+ * <p>A subsystem to contain the sensors required by Drive and Shooter</p>
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import com.sun.squawk.util.MathUtils;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+
+
+public class SharedSensors extends Subsystem
+{
+    private AnalogChannel leftUltrasonic, rightUltrasonic;
+    private DigitalInput leftLineReader, rightLineReader;
+    
+    private static final double DISTANCE_BETWEEN_ULTRASONICS = 19; //inches
+    
+    public SharedSensors(int leftUltraPort, int rightUltraPort, int leftReaderPort, int rightReaderPort)
+    {
+        leftUltrasonic = new AnalogChannel(leftUltraPort);
+        rightUltrasonic = new AnalogChannel(rightUltraPort);
+        
+        leftLineReader = new DigitalInput(leftReaderPort);
+        rightLineReader = new DigitalInput(rightReaderPort);
+    }
+    
+    public void initDefaultCommand()
+    {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    /**
+     * This method returns the 
+     * @return 
+     */
+    public double getLeftUltrasonicDistance()
+    {
+        return leftUltrasonic.getValue()/2;
+    }
+    
+    public double getRightUltrasonicDistance()
+    {
+        return rightUltrasonic.getValue()/2;
+    }
+    
+    public double getAverageUltrasonicDistance()
+    {
+        return  ( getLeftUltrasonicDistance() + getRightUltrasonicDistance() ) / 2;
+    }
+    
+    public double inferAngleFromUltrasonics()
+    {
+        // tan^-1(distance between ultras/difference between readings)
+        return MathUtils.atan(DISTANCE_BETWEEN_ULTRASONICS /
+                (getRightUltrasonicDistance() - getLeftUltrasonicDistance()));
+    }
+    
+}
